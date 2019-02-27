20fe4f1de7727f7c75a216cb864e0afa89f205fc
danvasey <31629758+danvasey@users.noreply.github.com>
2019-02-26 20:33:37 -0500

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,7 +1,40 @@
 <?php
+//
+// SESSIONS are like Cookies, they can hold information about specific users between pages 
+// to use them everypage you need to access the $_SESSION on you need to include session_start(); 
+// 
+
 session_start();
 
+
+
 if(isset($_POST['username'])){
+    
+                /*
+                /   utils is a class that has static calls [ie the ' :: '] You can call a static function without 
+                /   Creating the object first. 
+                
+                Example
+                    
+                        class object(){
+                            public static function MyStaticFunction(){ echo 'hello static';}
+                            public function Myfunction(){echo 'hello';}
+                        }
+                        
+                        $object = new object(); 
+                        $object->Myfunction(); 
+                            ***output*** 
+                            hello 
+                              *** END output*** 
+                            
+                     or without making the object first
+                            object::MyStaticFunction; 
+                            **** output *****
+                                Hello Static 
+                            *** END output*** 
+                /
+                */
+    
     $username = utils::getURLParam($_POST, 'username');
     $password = utils::getURLParam($_POST, 'password'); 
     
@@ -13,9 +46,13 @@ if(isset($_POST['username'])){
     if(!$login){
         die('Login Failed');}
     
+           
+    
+    
         $_SESSION['userName'] = $user->name;
         $_SESSION['userID'] = $user->id;
         
+    // Header can only be called if you don't output anything to the page first. 
     header('location:controllers/home.php');
 
 }
